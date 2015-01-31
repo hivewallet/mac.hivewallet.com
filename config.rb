7e646275caaf0c604f4ddf462bcc3ecc6428f668
Jakub Suder <jakub.suder@gmail.com>
2015-01-31 14:08:18 +0100

    config updates

--- config.rb
+++ config.rb
@@ -1,12 +1,8 @@
+activate :directory_indexes
+
 configure :build do
   activate :minify_css
-  # activate :minify_javascript
+  activate :minify_javascript
 
   activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
